2b525d9eaed3d68c95144187debe463a1621d77c
Ramkumar <ramkumar@Ramkumars-MacbookPro.local>
2016-03-13 08:28:05 +0530

    Version 0.0.13
    Security Improvement
    - Using appendChild method instead of innerHTML to prevent a possible XSS attack

--- data/js/bookmarks.js
+++ data/js/bookmarks.js
@@ -1,5 +1,4 @@
-self.port.emit("get_bookmarks");
-self.port.on("bookmarks", function(bookmarks) {
+function prepare_html(bookmarks) {
   if (localStorage.getItem("show_bookmarks") === null)
   {
     localStorage.setItem("show_bookmarks", "true");
@@ -7,8 +6,7 @@ self.port.on("bookmarks", function(bookmarks) {
   show_bookmarks = localStorage.getItem("show_bookmarks");
   if (show_bookmarks === 'true'){
     var it3 = '';
-    console.log(bookmarks);
-    it3 += '<hr/><center><h3><u>Bookmarks</u></h3></center>';
+    it3 += '<hr></hr><center><h3><u>Bookmarks</u></h3></center>';
     for (var i = 0, c = 0; i < bookmarks.length; i++) {
       if(c % 6 === 0)
       {
@@ -23,7 +21,10 @@ self.port.on("bookmarks", function(bookmarks) {
         it3+='<div class="col-xs-6 col-sm-3 col-md-2 col-lg-2"><a href="'+bookmarks[i].url+'" id="'+bookmarks[i].title+'" class="thumbnail" title="'+bookmarks[i].title+'"><table><tr><td class="td"><object width="32" height="32" data="'+url.origin+'/favicon.ico'+'"><img width="35" height="35" src = "images/default.png"/></object></td><td class="td">'+bookmarks[i].title+'</td></tr></table></a></div>';
       }
     }
-    document.getElementById("bookmarks").innerHTML = it3;
-
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(it3, "text/html");
+    document.getElementById('bookmarks').appendChild(doc.firstChild);
   }
-});
+}
+self.port.emit("get_bookmarks");
+self.port.on("bookmarks", prepare_html);
